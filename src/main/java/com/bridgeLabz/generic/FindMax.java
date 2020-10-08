2d34c179b9d646717021fef7a73fc57610cc48c2
unknown <ompreetam317@gmail.com>
2020-10-08 22:34:38 +0530

    [Preetam]Added: Extended Max function to Take More than 3 Parameters

--- src/main/java/com/bridgeLabz/generic/FindMax.java
+++ src/main/java/com/bridgeLabz/generic/FindMax.java
@@ -1,37 +1,25 @@
 package com.bridgeLabz.generic;
 
-public class FindMax<X , Y , Z > {
+import java.util.Arrays;
+
+public class FindMax<X, Y, Z> {
 	X[] xArr;
 	Y[] yArr;
 	Z[] zArr;
 	
+	public FindMax() {}
 	public FindMax(X[] xArr, Y[] yArr, Z[] zArr) {
-		
+
 		this.xArr = xArr;
 		this.yArr = yArr;
 		this.zArr = zArr;
 	}
 
-	public <T extends Comparable<T>> T Max(T[] arr) {
-		T max = arr[0];
-		if (arr[1].compareTo(max) > 0) {
-			max = arr[1];
-		}
-		if (arr[2].compareTo(max) > 0) {
-			max = arr[2];
-		}
-
+	public <T extends Comparable<T>> T Max(T... optionalValues) {
+		Arrays.sort(optionalValues);
+		T max = optionalValues[optionalValues.length-1];
 		return max;
 
 	}
 
-	public static void main(String[] args) {
-		Integer[] xArr = { 3, 2, 1 };
-		Float[] yArr = { 3.1f, 2.9f, 1.0f };
-		String[] zArr = { "peach", "apple", "banana" };
-		
-		FindMax findMax = new FindMax<Integer,Float,String>(xArr,yArr,zArr);
-		Comparable m = 3;
-		System.out.println(findMax.Max(xArr)==m);
-	}
 }
--- src/test/java/com/bridgeLabz/generic/GenericTest.java
+++ src/test/java/com/bridgeLabz/generic/GenericTest.java
@@ -2,6 +2,7 @@ package com.bridgeLabz.generic;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -11,22 +12,52 @@ public class GenericTest {
 	/**
 	 * Rigorous Test :-)
 	 */
-	
+	FindMax findMax;
+	@Before
+	public void initialize() {
+		findMax = new FindMax();
+	}
 
 	@Test
 	public void GivenIntegerArrayWithMaxAtFirst_returnsCorrectMax_shouldAnswerWithTrue() {
-		Integer[] xArr = { 3, 2, 1 };
-		Float[] yArr = { 3.1f, 2.9f, 1.0f };
-		String[] zArr = { "peach", "apple", "banana" };
-		FindMax findMax = new FindMax<Integer,Float,String>(xArr,yArr,zArr);
-		Integer xArrMax = (Integer)findMax.Max(xArr);
-		Float yArrMax = (Float)findMax.Max(yArr);
-		String zArrMax = (String)findMax.Max(zArr);
-		assertTrue(xArrMax==3);
-		assertTrue(yArrMax==3.1f);
-		assertTrue(zArrMax.matches("peach"));
 		
-		
+		Integer xArrMax = (Integer) findMax.Max(99, 3, 2, 1);
+		Float yArrMax = (Float) findMax.Max(9.88f, 3.1f, 2.9f, 1.0f);
+		String zArrMax = (String) findMax.Max("zucchini", "peach", "apple", "banana");
+		assertTrue(xArrMax == 99);
+		assertTrue(yArrMax == 9.88f);
+		assertTrue(zArrMax.matches("zucchini"));}
+
+	@Test
+	public void GivenIntegerArrayWithMaxAtSecond_returnsCorrectMax_shouldAnswerWithTrue() {
+		Integer xArrMax = (Integer) findMax.Max(3, 99, 2, 1);
+		Float yArrMax = (Float) findMax.Max(3.1f, 9.88f, 2.9f, 1.0f);
+		String zArrMax = (String) findMax.Max("peach", "zucchini", "apple", "banana");
+		assertTrue(xArrMax == 99);
+		assertTrue(yArrMax == 9.88f);
+		assertTrue(zArrMax.matches("zucchini"));
+
 	}
 
+	@Test
+	public void GivenIntegerArrayWithMaxAtThird_returnsCorrectMax_shouldAnswerWithTrue() {
+		Integer xArrMax = (Integer) findMax.Max(2, 3, 99, 1);
+		Float yArrMax = (Float) findMax.Max(2.9f, 3.1f, 9.88f, 1.0f);
+		String zArrMax = (String) findMax.Max("apple", "peach", "zucchini", "banana");
+		assertTrue(xArrMax == 99);
+		assertTrue(yArrMax == 9.88f);
+		assertTrue(zArrMax.matches("zucchini"));
+
+	}
+
+	@Test
+	public void GivenIntegerArrayWithMaxAtfourth_returnsCorrectMax_shouldAnswerWithTrue() {
+		Integer xArrMax = (Integer) findMax.Max(1, 2, 3, 99);
+		Float yArrMax = (Float) findMax.Max(1.0f, 2.9f, 3.1f, 9.88f);
+		String zArrMax = (String) findMax.Max("banana", "apple", "peach", "zucchini");
+		assertTrue(xArrMax == 99);
+		assertTrue(yArrMax == 9.88f);
+		assertTrue(zArrMax.matches("zucchini"));
+
+	}
 }
