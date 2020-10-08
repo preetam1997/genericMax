e557b4e33ae7f575fe192dd72031169c5588af29
unknown <ompreetam317@gmail.com>
2020-10-08 20:04:43 +0530

    [Preetam]Refactored: master, Created a generic class to take in 3 variables of generic Type

--- src/main/java/com/bridgeLabz/generic/FindMax.java
+++ src/main/java/com/bridgeLabz/generic/FindMax.java
@@ -1,6 +1,16 @@
 package com.bridgeLabz.generic;
 
-public class FindMax {
+public class FindMax<X , Y , Z > {
+	X[] xArr;
+	Y[] yArr;
+	Z[] zArr;
+	
+	public FindMax(X[] xArr, Y[] yArr, Z[] zArr) {
+		
+		this.xArr = xArr;
+		this.yArr = yArr;
+		this.zArr = zArr;
+	}
 
 	public <T extends Comparable<T>> T Max(T[] arr) {
 		T max = arr[0];
@@ -15,5 +25,13 @@ public class FindMax {
 
 	}
 
-
+	public static void main(String[] args) {
+		Integer[] xArr = { 3, 2, 1 };
+		Float[] yArr = { 3.1f, 2.9f, 1.0f };
+		String[] zArr = { "peach", "apple", "banana" };
+		
+		FindMax findMax = new FindMax<Integer,Float,String>(xArr,yArr,zArr);
+		Comparable m = 3;
+		System.out.println(findMax.Max(xArr)==m);
+	}
 }
--- src/test/java/com/bridgeLabz/generic/GenericTest.java
+++ src/test/java/com/bridgeLabz/generic/GenericTest.java
@@ -11,75 +11,22 @@ public class GenericTest {
 	/**
 	 * Rigorous Test :-)
 	 */
-	@Test
-	public void GivenIntegerArrayWithMaxAtThird_returnsCorrectMax_shouldAnswerWithTrue() {
-		Integer[] arr = { 1, 2, 3 };
-		FindMax findMax = new FindMax();
-		Integer max = findMax.Max(arr);
-		assertTrue(max == 3);
-	}
-
-	@Test
-	public void GivenIntegerArrayWithMaxAtSecond_returnsCorrectMax_shouldAnswerWithTrue() {
-		Integer[] arr = { 1, 3, 2 };
-		FindMax findMax = new FindMax();
-		Integer max = findMax.Max(arr);
-		assertTrue(max == 3);
-	}
+	
 
 	@Test
 	public void GivenIntegerArrayWithMaxAtFirst_returnsCorrectMax_shouldAnswerWithTrue() {
-		Integer[] arr = { 3, 2, 1 };
-		FindMax findMax = new FindMax();
-		Integer max = findMax.Max(arr);
-		assertTrue(max == 3);
+		Integer[] xArr = { 3, 2, 1 };
+		Float[] yArr = { 3.1f, 2.9f, 1.0f };
+		String[] zArr = { "peach", "apple", "banana" };
+		FindMax findMax = new FindMax<Integer,Float,String>(xArr,yArr,zArr);
+		Integer xArrMax = (Integer)findMax.Max(xArr);
+		Float yArrMax = (Float)findMax.Max(yArr);
+		String zArrMax = (String)findMax.Max(zArr);
+		assertTrue(xArrMax==3);
+		assertTrue(yArrMax==3.1f);
+		assertTrue(zArrMax.matches("peach"));
+		
+		
 	}
 
-	@Test
-	public void GivenFloatArrayWithMaxAtFirst_returnsCorrectMax_shouldAnswerWithTrue() {
-		Float[] arr = { 3.1f, 2.9f, 1.0f };
-		FindMax findMax = new FindMax();
-		Float max = findMax.Max(arr);
-		assertTrue(max == 3.1f);
-	}
-
-	@Test
-	public void GivenFloatArrayWithMaxAtSecond_returnsCorrectMax_shouldAnswerWithTrue() {
-		Float[] arr = { 1.0f, 3.1f, 2.9f };
-		FindMax findMax = new FindMax();
-		Float max = findMax.Max(arr);
-		assertTrue(max == 3.1f);
-	}
-
-	@Test
-	public void GivenFloatArrayWithMaxAtThird_returnsCorrectMax_shouldAnswerWithTrue() {
-		Float[] arr = { 2.9f, 1.0f, 3.1f };
-		FindMax findMax = new FindMax();
-		Float max = findMax.Max(arr);
-		assertTrue(max == 3.1f);
-	}
-
-	@Test
-	public void GivenStringArrayWithMaxAtFirst_returnsCorrectMax_shouldAnswerWithTrue() {
-		String[] arr = { "peach", "apple", "banana" };
-		FindMax findMax = new FindMax();
-		String max = findMax.Max(arr);
-		assertTrue(max.matches("peach"));
-	}
-
-	@Test
-	public void GivenStringArrayWithMaxAtSecond_returnsCorrectMax_shouldAnswerWithTrue() {
-		String[] arr = { "apple", "peach", "banana" };
-		FindMax findMax = new FindMax();
-		String max = findMax.Max(arr);
-		assertTrue(max.matches("peach"));
-	}
-
-	@Test
-	public void GivenStringArrayWithMaxAtThird_returnsCorrectMax_shouldAnswerWithTrue() {
-		String[] arr = { "apple", "banana", "peach" };
-		FindMax findMax = new FindMax();
-		String max = findMax.Max(arr);
-		assertTrue(max.matches("peach"));
-	}
 }
