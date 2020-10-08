53d38aed81cf9855eeeeed4606a2143c720708a7
unknown <ompreetam317@gmail.com>
2020-10-08 19:27:46 +0530

    [Preetam]Refactored: All the three method into one generic method

--- src/main/java/com/bridgeLabz/generic/FindMax.java
+++ src/main/java/com/bridgeLabz/generic/FindMax.java
@@ -2,8 +2,8 @@ package com.bridgeLabz.generic;
 
 public class FindMax {
 
-	public Integer Max(Integer[] arr) {
-		int max = arr[0];
+	public <T extends Comparable<T>> T Max(T[] arr) {
+		T max = arr[0];
 		if (arr[1].compareTo(max) > 0) {
 			max = arr[1];
 		}
@@ -15,30 +15,5 @@ public class FindMax {
 
 	}
 
-	public Float Max(Float[] arr) {
-		float max = arr[0];
-		if (arr[1].compareTo(max) > 0) {
-			max = arr[1];
-		}
-		if (arr[2].compareTo(max) > 0) {
-			max = arr[2];
-		}
-
-		return max;
-
-	}
-	
-	public String Max(String[] arr) {
-		String max = arr[0];
-		if (arr[1].compareTo(max) > 0) {
-			max = arr[1];
-		}
-		if (arr[2].compareTo(max) > 0) {
-			max = arr[2];
-		}
-
-		return max;
-
-	}
 
 }
